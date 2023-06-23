4868f0d0f407d382d35655480e6e913b86192926
RAHUL <rahulmeetawa@gmail.com>
2023-06-23 12:56:23 +0530

    fix somethings

--- src/components/UserReport.jsx
+++ src/components/UserReport.jsx
@@ -3,16 +3,20 @@ import React from "react";
 const UserReport = ({ filteredUsers }) => {
   return (
     <div className="outer-div">
-      {filteredUsers.map((user) => (
-        <>
-          <div className="inner-div">
-            <img src={user.avatar} alt={user.first_name} />
-          </div>
-          <p>
-            {user.first_name} {user.last_name}
-          </p>
-        </>
-      ))}
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>
+            <div>
+              <div className="inner-div">
+                <img src={user.avatar} alt={user.first_name} />
+              </div>
+              <p>
+                {user.first_name} {user.last_name}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
