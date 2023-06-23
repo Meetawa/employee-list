3401b3c8845446b81a2a5353f220da484f68178d
RAHUL <rahulmeetawa@gmail.com>
2023-06-23 12:44:50 +0530

    fix somethings

--- src/components/UserReport.jsx
+++ src/components/UserReport.jsx
@@ -4,10 +4,14 @@ const UserReport = ({ filteredUsers }) => {
   return (
     <div className="outer-div">
       {filteredUsers.map((user) => (
-        <div className="inner-div">
-          <img src={user.avatar} alt={user.first_name} />
-          <p>{user.first_name}</p>
-        </div>
+        <>
+          <div className="inner-div">
+            <img src={user.avatar} alt={user.first_name} />
+          </div>
+          <p>
+            {user.first_name} {user.last_name}
+          </p>
+        </>
       ))}
     </div>
   );
