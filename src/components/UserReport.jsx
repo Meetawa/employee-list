a16275e651ba26512e4f0975bd20adf19076015b
RAHUL <rahulmeetawa@gmail.com>
2023-06-23 11:20:18 +0530

    add some class

--- src/components/UserReport.jsx
+++ src/components/UserReport.jsx
@@ -2,19 +2,13 @@ import React from "react";
 
 const UserReport = ({ filteredUsers }) => {
   return (
-    <div>
-      {" "}
-      <ul>
-        {filteredUsers.map((user) => (
-          <li key={user.id}>
-            <div>
-              <p>ID: {user.id}</p>
-              <p>First Name: {user.first_name}</p>
-              <img src={user.avatar} alt={user.first_name} />
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className="outer-div">
+      {filteredUsers.map((user) => (
+        <div className="inner-div">
+          <img src={user.avatar} alt={user.first_name} />
+          <p>{user.first_name}</p>
+        </div>
+      ))}
     </div>
   );
 };
